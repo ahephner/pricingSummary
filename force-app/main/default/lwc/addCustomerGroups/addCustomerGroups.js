4147fc7928b9a2de27d1ec52b073a4310d3e1a87
ahephner <ajhephner178@gmail.com>
2024-12-05 10:19:54 -0500

    can create shared price book and member group work on getting all price books now

--- force-app/main/default/lwc/addCustomerGroups/addCustomerGroups.js
+++ force-app/main/default/lwc/addCustomerGroups/addCustomerGroups.js
@@ -1,18 +1,29 @@
 import { api } from 'lwc';
 import LightningModal from 'lightning/modal';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import createSharePrice from '@salesforce/apex/omsOppUpdatePricing.createSharePriceBook';
 export default class AddCustomerGroups extends LightningModal {
     @api content
     priceBookDropStyle = 'slds-listbox slds-listbox_vertical slds-dropdown noOffSet'
     searchLabel = 'Search Shared Price Books'
     showPriceBooks = true; 
+    loaded = true; 
     createNewPriceBook(){
         this.showPriceBooks = false; 
     }
     async createPriceBook(){
+            this.loaded = false; 
             let x = this.refs.pbName.value;
             let create = await createSharePrice({name:x}) 
-            console.log(create); 
+            if(create.includes('success')){
+                let mess = create.replace('success ', '')
+                //toast does not work in modal
+            }else{
+                //toast does not work 
+                console.error('on create => ', create)
+                  
+            }
+            this.loaded = true; 
             this.showPriceBooks = true;
         }
     cancelPriceBook(){
